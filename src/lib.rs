2dd5acbd049cbe944516678b0676e04539805d10
yangxu52 <yyswj10@163.com>
2024-06-14 16:48:01 +0800

    feat✨: output error msg to console, when panic

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
+mod error;
 mod rsa;
 mod sha2;
+
+extern crate console_error_panic_hook;
+use wasm_bindgen::prelude::*;
+
+use console_error_panic_hook::set_once as set_panic_hook;
+
+#[wasm_bindgen(start)]
+pub fn hook() {
+    set_panic_hook();
+}
